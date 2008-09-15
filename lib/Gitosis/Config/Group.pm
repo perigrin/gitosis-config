a0068910a3e363f10b3f9ce784aa76788089ba53
Chris Prather <chris@prather.org>
2008-09-14 21:23:49 -0400

    forgot the Group class

---
+++ lib/Gitosis/Config/Group.pm
@@ -0,0 +1,22 @@
+package Gitosis::Config::Group;
+use Moose;
+use Moose::Util::TypeConstraints;
+
+has [qw(name)] => (
+    isa => 'Str',
+    is  => 'ro',
+);
+
+subtype 'Gitosis::Config::Group::List' => as 'ArrayRef';
+coerce 'Gitosis::Config::Group::List' => from 'Str' => via {
+    [ split /\s+/, $_ ];
+};
+
+has [qw(writable members)] => (
+    isa    => 'Gitosis::Config::Group::List',
+    is     => 'ro',
+    coerce => 1,
+);
+
+no Moose;
+1;
